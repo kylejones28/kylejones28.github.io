e66eb2114cc83e323e9b5def62564a90680b7bcf
kylejones28 <81721243+kylejones28@users.noreply.github.com>
2021-04-29 00:14:20 -0600

    Add files via upload

---
+++ StudentLabs/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name:Kyle Jones
+    File Name: script.js
+    Date: 4/28/2021
+*/
+
+//jQuery for hero image to consume the header window space
+
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
